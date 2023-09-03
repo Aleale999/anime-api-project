aa62305c0e6f49eb42ef0e1f885ab336e026a6df
Aleale999 <alebatti2710gmail.com>
2023-09-03 18:07:44 +0200

    Polished sorting by genre and added pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,11 +14,16 @@ export default function Home(){
   const [allGenres, setAllGenres] = useState()
   const [genre, setGenre] = useState('All')
 
+  let currentPage = 1
+  const [lastPage, setLastPage] = useState()
+  const [pageNumber, setPageNumber] = useState(1)
+
   useEffect(() => {
     async function getAnimeGenres(){
       try {
         const { data } = await axios.get('https://api.jikan.moe/v4/genres/anime')
         const names = data.data
+        
         setAnimeGenreId(names.map(id => id.mal_id))
         setAllGenres(names.map(genre => genre.name))
       } catch (err){
@@ -31,18 +36,51 @@ export default function Home(){
   useEffect(()=> {
     async function searchGenres(){
       if (genre !== 'All') {
-        const { data } = await axios.get(`https://api.jikan.moe/v4/anime?genres=${genre}`)
+        const { data } = await axios.get(`https://api.jikan.moe/v4/anime?genres=${genre}&page=${pageNumber}`)
         setNewAnimeNames(data.data.map(name=>name))
       } else {
-        const { data } = await axios.get('https://api.jikan.moe/v4/anime')
+        const { data } = await axios.get(`https://api.jikan.moe/v4/anime?page=${pageNumber}`)
         setAnimeNames(data.data.map(name=>name))
       }
     }
     searchGenres()
-  }, [genre])
+  }, [genre,pageNumber])
+  
+  function changePage(target){
+    console.log(target)
+    currentPage = pageNumber
+    if (target === '1') {
+      currentPage--
+    } else if (target === '2' && currentPage !== 1) {
+      currentPage++
+    } else {
+      currentPage = 2
+    }
+    setPageNumber(currentPage)
+  }
+  useEffect(() => {
+    function resetPage(){
+      currentPage = 1
+      setPageNumber(currentPage)
+    }
+    resetPage()
+  },[genre])
+
+  useEffect(() => {
+    async function getLastPage(){
+      if (genre !== 'All') {
+        const { data } = await axios.get(`https://api.jikan.moe/v4/anime?genres=${genre}`)
+        setLastPage(data.pagination.last_visible_page)
+      }
+    }
+    getLastPage()
+  },[genre])
 
   return (
     <>
+      <button disabled={pageNumber === 1 ? true : false} value={1} onClick={(e) => changePage(e.target.value)}>Previous-Page</button>
+      <p>{pageNumber}</p>
+      <button disabled={pageNumber === lastPage ? true : false} value={2} onClick={(e) => changePage(e.target.value)}>Next-Page</button>
       {
         <select onChange={((e) => setGenre(e.target.value))}>
           <option value="All">All</option>
@@ -77,8 +115,9 @@ export default function Home(){
         })
         }
       </span>
-      <button><Link to='/'>Previous-Page</Link></button>
-      <button><Link to='/'>Next-Page</Link></button>
+      <button disabled={pageNumber === 1 ? true : false} value={1} onClick={(e) => changePage(e.target.value)}>Previous-Page</button>
+      <p>{pageNumber}</p>
+      <button disabled={pageNumber === lastPage ? true : false} value={2} onClick={(e) => changePage(e.target.value)}>Next-Page</button>
     </>
   )
 }
