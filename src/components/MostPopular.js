46a4f28778f89df391e4c1f7efd552315f63480f
Aleale999 <alebatti2710gmail.com>
2023-09-04 13:50:04 +0200

    Added Most watched

--- src/components/MostPopular.js
+++ src/components/MostPopular.js
@@ -1,5 +1,35 @@
+import { useEffect,useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
+export default function Mostpopular(){
 
-export default function MostPopular(){
-  
+  const [popular, setPopular] = useState()
+  const [animeId, setAnimeId] = useState()
+
+  useEffect(() => {
+    async function popularAnimes(){
+      const { data } = await axios.get('https://api.jikan.moe/v4/anime?order_by=popularity')
+      setPopular(data.data.map(name=>name))
+      setAnimeId(data.data.map(id => id.mal_id))
+    }
+    popularAnimes()
+  }, [])
+
+  return (
+    <>
+      {popular && popular.map(({ title, images }, i) => {
+        return (
+          <span
+            key={i} value={title} className='single-anime-container'>
+            <Link to={`/${animeId[i]}`} >
+              <span className='anime-img'><img src={images.jpg.image_url}/></span>
+              <h3 className='anime-title'>{title}</h3>
+            </Link>
+        
+          </span>
+        )
+      })}
+    </>
+  )
 }
