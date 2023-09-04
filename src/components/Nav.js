26ea9ebb8a6952cf4076921746afd2557e88acc1
Aleale999 <alebatti2710gmail.com>
2023-09-04 15:29:57 +0200

    Added styling and everything

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,9 +7,12 @@ export default function Nav(){
 
   return (
     <>
-      <Link to='/MostWatched'>Most Watched</Link>
-      <Link to='/MostPopular'>Most Popular</Link>
-      <Link to='/LowestScore'>Lowest Score</Link>
+      <section className='topnav'>
+        <Link to='/'className='home'>Home</Link>
+        <Link to='/MostWatched'className='mostWatched'>Most Watched</Link>
+        <Link to='/MostPopular'className='mostPopular'>Most Popular</Link>
+        <Link to='/LowestScore'className='lowestScore'>Lowest Score</Link>
+      </section>
     </>
   )
 }
